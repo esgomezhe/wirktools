b83b1ec960979d73f541bcc9e02426e5c34344d1
Juan Esteban Jiménez Daza <juanes.pio51@gmail.com>
2024-03-25 15:55:33 -0500

    añadiendo formulario como dropdown, y modificando navbar en moviles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,112 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+  useEffect(() => {
+    /**
+   * Mobile nav toggle
+   */
+    const navbar = document.getElementById('navbar');
+
+    const handleMobileNavToggle = (e) => {
+      navbar.classList.toggle('navbar-mobile');
+      e.target.classList.toggle('bi-list');
+      e.target.classList.toggle('bi-x');
+    };
+
+    /**
+   * Mobile nav dropdowns activate
+   */
+    const handleDropdownClick = (e) => {
+      if (navbar.classList.contains('navbar-mobile')) {
+        e.preventDefault();
+        e.target.nextElementSibling?.classList.toggle('dropdown-active'); // Added optional chaining
+      }
+    };
+
+    /**
+    * Scrool with ofset on links with a class name .scrollto
+       */
+    const handleScrollTo = (hash) => {
+      const targetElement = document.querySelector(hash);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+
+    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+    if (mobileNavToggle) {
+      mobileNavToggle.addEventListener('click', handleMobileNavToggle);
+    }
+
+    const dropdownLinks = document.querySelectorAll('.navbar .dropdown > a');
+    if (dropdownLinks) {
+      dropdownLinks.forEach(item => {
+        item.addEventListener('click', handleDropdownClick, true);
+      });
+    }
+
+    const scrollToLinks = document.querySelectorAll('.scrollto');
+    if (scrollToLinks) {
+      scrollToLinks.forEach(item => {
+        item.addEventListener('click', (e) => handleScrollTo(e.target.hash), true);
+      });
+    }
+
+    return () => {
+      if (mobileNavToggle) {
+        mobileNavToggle.removeEventListener('click', handleMobileNavToggle);
+      }
+      if (dropdownLinks) {
+        dropdownLinks.forEach(item => {
+          item.removeEventListener('click', handleDropdownClick, true);
+        });
+      }
+      if (scrollToLinks) {
+        scrollToLinks.forEach(item => {
+          item.removeEventListener('click', (e) => handleScrollTo(e.target.hash), true);
+        });
+      }
+    };
+  }, []);
+
+
   return (
     <nav id="navbar" className="navbar">
       <ul>
-        <li><Link className="active" to='/' > Home</Link></li>
-        <li><Link to='/formulario' > Formulario</Link></li>
-        <li><Link to='/about' > About</Link></li>
-        <li><Link to='/services' > Services</Link></li>
-        <li><Link to='/portfolio' > Portfolio</Link></li>
-        <li><Link to='/team' > Team</Link></li>
-        <li><Link to='/blog' > Blog</Link></li>
-        <li className="dropdown"><Link to='#' > <span>Drop Down</span><i className="bi bi-chevron-down"></i></Link>
+        <li><Link className="active" to='/'>Home</Link></li>
+        <li className="dropdown"><Link to='#'><span>Formulario</span> <i className="bi bi-chevron-down"></i></Link>
           <ul>
-            <li><Link to='#' >Drop Down 1</Link></li>
-            <li className="dropdown"><Link to='#' ><span>Deep Drop Down</span><i className="bi bi-chevron-right"></i></Link>
-              <ul>
-                <li><Link to='#' >Deep Drop Down 1</Link></li>
-                <li><Link to='#' >Deep Drop Down 2</Link></li>
-                <li><Link to='#' >Deep Drop Down 3</Link></li>
-                <li><Link to='#' >Deep Drop Down 4</Link></li>
-                <li><Link to='#' >Deep Drop Down 5</Link></li>
-              </ul>
-            </li>
-            <li><Link to='#' >Drop Down 2</Link></li>
-            <li><Link to='#' >Drop Down 3</Link></li>
-            <li><Link to='#' >Drop Down 4</Link></li>
+            <li><Link to='/formularioQ'>Formulario</Link></li>
+            <li><Link to='/formularioQ'>Formulario 2</Link></li>
+            <li><Link to='/formularioQ'>Formulario 3</Link></li>
+            <li><Link to='/formularioQ'>Formulario 4</Link></li>
           </ul>
         </li>
-        <li><Link to='/contact' > Contact Us</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/services'>Services</Link></li>
+        <li><Link to='/portfolio'>Portfolio</Link></li>
+        <li><Link to='/team'>Team</Link></li>
+        <li><Link to='/blog'>Blog</Link></li>
+        <li className="dropdown"><Link to='#'><span>Drop Down</span><i className="bi bi-chevron-down"></i></Link>
+          <ul>
+            <li><Link to='#'>Drop Down 1</Link></li>
+            <li className="dropdown"><Link to='#'><span>Deep Drop Down</span><i className="bi bi-chevron-right"></i></Link>
+              <ul>
+                <li><Link to='#'>Deep Drop Down 1</Link></li>
+                <li><Link to='#'>Deep Drop Down 2</Link></li>
+                <li><Link to='#'>Deep Drop Down 3</Link></li>
+                <li><Link to='#'>Deep Drop Down 4</Link></li>
+                <li><Link to='#'>Deep Drop Down 5</Link></li>
+              </ul>
+            </li>
+            <li><Link to='#'>Drop Down 2</Link></li>
+            <li><Link to='#'>Drop Down 3</Link></li>
+            <li><Link to='#'>Drop Down 4</Link></li>
+          </ul>
+        </li>
+        <li><Link to='/contact'>Contact Us</Link></li>
       </ul>
       <i className="bi bi-list mobile-nav-toggle"></i>
     </nav>
--- frontend/src/pages/FormularioPage.jsx
+++ frontend/src/pages/FormularioPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Formulario from "../components/Formulario";
 import LandingPageForm from "../components/LandingPageForm";
 
 function FormularioPage() {
