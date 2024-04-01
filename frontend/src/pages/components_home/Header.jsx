b733685ea40338358d2125f6bdbbab5540fb70f6
Juan Esteban Jiménez Daza <juanes.pio51@gmail.com>
2024-04-01 12:39:42 -0500

    añadiendo navbar al home en about

--- frontend/src/pages/components_home/Header.jsx
+++ frontend/src/pages/components_home/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "../stylesheets/header.css";
+import Navbar from "./Navbar";
 
 function Header() {
   return (
     <>
-      <header>
-        <div className="header">
+      <header className="header">
+        <div className="header-container">
+          <div className="header-logo">
+            <img className="header-image" src="https://transformaciondigital.com.co/wp-content/uploads/2023/03/Logo-ccc-1-e1696351300230.png" alt="Camara de comercio de cali" />
 
+            <img className="header-image" src="https://transformaciondigital.com.co/wp-content/uploads/2023/03/Logo.png" alt="wirk consulting" />
+          </div>
+
+          <Navbar />
         </div>
       </header>
     </>
---
+++ frontend/src/pages/components_home/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/navbar.css";
+
+function Navbar() {
+  const [openDropdown, setOpenDropdown] = useState(null);
+
+  const handleMouseEnter = (dropdown) => {
+    setOpenDropdown(dropdown);
+  };
+
+  const handleMouseLeave = () => {
+    setOpenDropdown(null);
+  };
+
+  return (
+    <nav className="navbar-container">
+      <ul className="navbar-menu">
+        <li className="navbar-item active">
+          <Link to="/" className="navbar-link active">
+            home
+          </Link>
+        </li>
+
+        <li
+          onMouseEnter={() => handleMouseEnter(1)}
+          onMouseLeave={handleMouseLeave}
+          className="navbar-item"
+        >
+          <div className="navbar-dropdown">
+            <span className="navbar-link">herramienta de autodiagnóstico <i className="bi bi-chevron-down" /></span>
+            <div className={`dropdown-content ${openDropdown === 1 ? 'open' : ''}`}>
+              <Link to="/formularioQ" className="dropdown-item">
+                formulario de caracterizacion
+              </Link>
+            </div>
+          </div>
+        </li>
+
+        <li
+          onMouseEnter={() => handleMouseEnter(2)}
+          onMouseLeave={handleMouseLeave}
+          className="navbar-item"
+        >
+          <div className="navbar-dropdown">
+            <span className="navbar-link">iniciativa cluster <i className="bi bi-chevron-down" /></span>
+            <div className={`dropdown-content ${openDropdown === 2 ? 'open' : ''}`}>
+              <Link to="#" className="dropdown-item">
+                formulario de caracterizacion
+              </Link>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
