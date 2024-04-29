a4ad5b515e815c6e802f30c34131cdbef6faf379
Juan Esteban Jiménez Daza <juanes.pio51@gmail.com>
2024-04-29 17:22:40 -0500

    Home 100% y reponsive web 100% (ajustar botones)

--- frontend/src/components/componentes_new_home/FooterHome.jsx
+++ frontend/src/components/componentes_new_home/FooterHome.jsx
@@ -8,62 +8,63 @@ import EmailJudicial from '../../img/email2_footer.svg'
 
 function FooterHome() {
   return (
-    <footer className='footer'>
-      <div className="footer__content">
-        <div className='footer__content--container'>
-          <div className="footer__section">
-            <div className='footer__logos'>
-              <img src={require('../../img/logo.jpg')} alt="camara de comercio cali logo" height={40} />
-              <div className="footer_logos--wirk">
-                <span className='wirk--span'>Con el apoyo de:</span>
-                <img src={require('../../img/wirk_logo.jpg')} alt="wirkconsulting logo" height={37} />
+    <>
+      <footer className='footer'>
+        <div className="footer__content">
+          <div className='footer__content--container'>
+            <div className="footer__section">
+              <div className='footer__logos'>
+                <img src={require('../../img/logo.jpg')} alt="camara de comercio cali logo" height={40} />
+                <div className="footer_logos--wirk">
+                  <span className='wirk--span'>Con el apoyo de:</span>
+                  <img src={require('../../img/wirk_logo.jpg')} alt="wirkconsulting logo" height={37} />
+                </div>
+              </div>
+
+              <div className='footer__direction'>
+                <span className='footer__direction--span'>cámara de comercio de cali</span>
+                <p className='footer__direction--text'>NIT: 890399001-1</p>
+                <p className='footer__direction--text'>Calle 8 No. 3 - 14</p>
+                <p className='footer__direction--text'>Cali, valle, colombia</p>
               </div>
             </div>
 
-            <div className='footer__direction'>
-              <span className='footer__direction--span'>cámara de comercio de cali</span>
-              <p className='footer__direction--text'>NIT: 890399001-1</p>
-              <p className='footer__direction--text'>Calle 8 No. 3 - 14</p>
-              <p className='footer__direction--text'>Cali, valle, colombia</p>
-            </div>
-          </div>
+            <div className='footer__contact'>
+              <div className='footer__contact--text'>
+                <img src={CallCenter} width={15} alt='call center' />
+                <div className='contact--text-container'>
+                  <span className='footer__contact--span'> call center: </span>
+                  <a href="tel:(602)8861300" className='footer__contact--link'>+57 (602) 8861300</a>
+                </div>
+              </div>
 
-          <div className='footer__contact'>
-            <div className='footer__contact--text'>
-              <img src={CallCenter} width={15} alt='call center' />
-              <div className='contact--text-container'>
-                <span className='footer__contact--span'> call center: </span>
-                <a href="tel:(602)8861300" className='footer__contact--link'>+57 (602) 8861300</a>
+              <div className='footer__contact--text'>
+                <img src={Email} width={15} alt='email' />
+                <div className='contact--text-container'>
+                  <span className='footer__contact--span'> email: </span>
+                  <a href="mailto:transformatedigital@ccc.org.co" className='footer__contact--link' >transformatedigital@ccc.org.co</a>
+                </div>
+              </div>
+
+              <div className='footer__contact--text'>
+                <img src={EmailJudicial} width={15} alt='notification email' />
+                <div className='contact--text-container'>
+                  <span className='footer__contact--span'> Notificaciones judiciales: </span>
+                  <a href="mailto:notificacionesjudiciales@ccc.org.co" className='footer__contact--link'>notificacionesjudiciales@ccc.org.co</a>
+                </div>
               </div>
             </div>
 
-            <div className='footer__contact--text'>
-              <img src={Email} width={15} alt='email' />
-              <div className='contact--text-container'>
-                <span className='footer__contact--span'> email: </span>
-                <a href="mailto:transformatedigital@ccc.org.co" className='footer__contact--link' >transformatedigital@ccc.org.co</a>
-              </div>
+            <div className="footer__tools">
+              <span className='footer__tools--span'>Autodiagnósticos disponibles</span>
+              <p className='footer__tools--text'>Medianas y grandes empresas</p>
+              <p className='footer__tools--text'>Micros y pequeñas empresas</p>
+              <p className='footer__tools--text'>Economía popular, asociativa y comunitaria</p>
+              <p className='footer__tools--text'>Excelencia clínica</p>
             </div>
-
-            <div className='footer__contact--text'>
-              <img src={EmailJudicial} width={15} alt='notification email' />
-              <div className='contact--text-container'>
-                <span className='footer__contact--span'> Notificaciones judiciales: </span>
-                <a href="mailto:notificacionesjudiciales@ccc.org.co" className='footer__contact--link'>notificacionesjudiciales@ccc.org.co</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="footer__tools">
-            <span className='footer__tools--span'>Autodiagnósticos disponibles</span>
-            <p className='footer__tools--text'>Medianas y grandes empresas</p>
-            <p className='footer__tools--text'>Micros y pequeñas empresas</p>
-            <p className='footer__tools--text'>Economía popular, asociativa y comunitaria</p>
-            <p className='footer__tools--text'>Excelencia clínica</p>
           </div>
         </div>
-      </div>
-
+      </footer>
       <div className="footer__socialmedia">
         <div className="footer__socialmedia--container">
           <div className="footer__socialmedia--links">
@@ -76,7 +77,7 @@ function FooterHome() {
           <p className='footer__socialmedia--text'>© 2024. Todos los derechos reservados</p>
         </div>
       </div>
-    </footer>
+    </>
   )
 }
 
--- frontend/src/components/componentes_new_home/ThirdSection.jsx
+++ frontend/src/components/componentes_new_home/ThirdSection.jsx
@@ -1,8 +1,124 @@
 import React from 'react'
+import Slider from "react-slick";
 import '../../stylesheets/newHome/thirdSection.css'
 import arrow from '../../img/arrow_portfolio.svg'
+import { useState, useEffect } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const HoverContent = ({ title, text, item1, item2, item3, item4 }) => (
+  <div className="hoverContent__item">
+    <div className='hoverContent__info--container'>
+      <h4 className='more--title'>{title}</h4>
+      <p className='more-text'>{text}</p>
+      <ul className='more-list'>
+        <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item1}</li>
+        <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item2}</li>
+        <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item3}</li>
+        <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item4}</li>
+      </ul>
+      <a href="#" className='button'> Conoce más</a>
+    </div>
+  </div>
+)
+
+const SliderItems = [
+  { title: 'presencia digital', text: 'Soluciones que potencian la visibilidad y participación de la empresa en el entorno en línea como:', item1: 'Diseño web', item2: 'Gestión de redes sociales', item3: 'Optimización de motores de búsqueda', item4: 'Email marketing' },
+
+  { title: 'Ventas por internet', text: 'Soluciones para la comercialización y venta de productos o servicios a través de plataformas en línea como:', item1: 'E-commerce', item2: 'Pasarelas de pago', item3: 'Mercados electrónicos', item4: 'CRM' },
+
+  { title: 'Digitalización de procesos', text: 'Soluciones para la automatización y optimización de los procesos comerciales mediante el uso de tecnología digital como:', item1: 'ERP', item2: 'POS', item3: 'Facturación electrónica', item4: 'Plataforma logística' }
+
+]
+
+var settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const SliderContent = () => (
+  <div className="slider-container">
+    <Slider {...settings}>
+      {SliderItems.map(item => (
+        <HoverContent
+          key={item.title}
+          title={item.title}
+          text={item.text}
+          item1={item.item1}
+          item2={item.item2}
+          item3={item.item3}
+          item4={item.item4}
+        />
+      ))}
+    </Slider>
+  </div>
+)
+
+const PortfolioItems = ({ image, span, title, text, item1, item2, item3, item4 }) => (
+  <div className={`portfolio__item ${image}`}>
+    <span className='portfolio__item--span'>{span}</span>
+    <div className="more__info">
+      <div className='more__info--container'>
+        <h4 className='more--title'>{title}</h4>
+        <p className='more-text'>{text}</p>
+        <ul className='more-list'>
+          <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item1}</li>
+          <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item2}</li>
+          <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item3}</li>
+          <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />{item4}</li>
+        </ul>
+        <a href="#" className='button'> Conoce más</a>
+      </div>
+    </div>
+  </div>
+)
+
+const PortfolioContent = [
+  { image: 'presence', span: 'Presencia Digital', title: 'presencia digital', text: 'Soluciones que potencian la visibilidad y participación de la empresa en el entorno en línea como', item1: 'Diseño web', item2: 'Gestión de redes sociales', item3: 'Optimización de motores de búsqueda', item4: 'Email marketing' },
+
+  { image: 'sells', span: 'Ventas por internet', title: 'Ventas por internet', text: 'Soluciones para la comercialización y venta de productos o servicios a través de plataformas en línea como:', item1: 'E-commerce', item2: 'Pasarelas de pago', item3: 'Pasarelas de pago', item4: 'CRM' },
+
+  { image: 'digitization', span: 'Digitalización de procesos', title: 'Digitalización de procesos', text: 'Soluciones para la automatización y optimización de los procesos comerciales mediante el uso de tecnología digital como:', item1: 'ERP', item2: 'POS', item3: 'Facturación electrónica', item4: 'Plataforma logística' }
+]
+
+const Portfolio = () => (
+  PortfolioContent.map(item => (
+    <PortfolioItems
+      key={item.title}
+      image={item.image}
+      span={item.span}
+      title={item.title}
+      text={item.text}
+      item1={item.item1}
+      item2={item.item2}
+      item3={item.item3}
+      item4={item.item4}
+    />
+  ))
+)
+
 
 function ThirdSection() {
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Limpiar el listener al desmontar el componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <section className='third__section'>
@@ -11,70 +127,10 @@ function ThirdSection() {
             <h3>Conoce las soluciones que tenemos para ti</h3>
             <p>Selecciona una categoría y conoce nuestro portafolio</p>
           </div>
-
-          <div className='third__section--portfolio'>
-
-            <div className='portfolio__item presence'>
-              <span className='portfolio__item--span'>Presencia Digital</span>
-
-              {/* lo que se mostrara cuando el contenedor tenga un hover */}
-              <div className="more__info">
-                <div className='more__info--container'>
-                  <h4 className='more--title'>presencia digital</h4>
-                  <p className='more-text'>Soluciones que potencian la visibilidad y participacion de la empresa en el entorno en linea como:</p>
-                  <ul className='more-list'>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Diseño web</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Optimización de motores de búsqueda</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Gestion de redes sociales</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Email marketing</li>
-                  </ul>
-                  <a href="#" className='button'> Conoce más</a>
-                </div>
-              </div>
-            </div>
-
-            <div className='portfolio__item sells'>
-              <span className='portfolio__item--span'>Ventas por internet</span>
-              {/* lo que se mostrara cuando el contenedor tenga un hover */}
-              <div className="more__info">
-                <div className='more__info--container'>
-                  <h4 className='more--title'>Ventas por internet</h4>
-                  <p className='more-text'>Soluciones para la comercialización y venta de productos o servicios a través de plataformas en línea como:</p>
-                  <ul className='more-list'>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />E-commerce</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Pasarelas de pago</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Mercados electrónicos</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />CRM</li>
-                  </ul>
-                  <a href="#" className='button'> Conoce más</a>
-                </div>
-              </div>
-            </div>
-
-            <div className='portfolio__item digitization'>
-              <span className='portfolio__item--span'>Digitalización de procesos</span>
-              {/* lo que se mostrara cuando el contenedor tenga un hover */}
-              <div className="more__info">
-                <div className='more__info--container'>
-                  <h4 className='more--title'>Digitalización de procesos</h4>
-                  <p className='more-text'>Soluciones para la automatización y optimización de los procesos comerciales mediante el uso de tecnología digital como:</p>
-                  <ul className='more-list'>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />ERP</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />POS</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Facturación electrónica</li>
-                    <li className='more-list--item'><img src={arrow} alt="arrow" style={{ marginRight: '10px', width: '16px' }} />Plataforma logística</li>
-                  </ul>
-                  <a href="#" className='button'> Conoce más</a>
-                </div>
-              </div>
-            </div>
-
-          </div>
+          {isMobile ? <SliderContent /> : <div className='third__section--portfolio'><Portfolio /> </div>}
         </div>
-
       </section>
     </>
   )
 }
-
 export default ThirdSection
