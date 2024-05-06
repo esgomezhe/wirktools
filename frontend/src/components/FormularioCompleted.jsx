9e6ae77958ec1be67e5358366465048f92388935
Esteban Gomez <esgomezhe@unal.edu.co>
2024-05-06 11:56:26 -0500

    Solución de bugs

--- frontend/src/components/FormularioCompleted.jsx
+++ frontend/src/components/FormularioCompleted.jsx
@@ -8,10 +8,12 @@ function FormularioCompleted() {
   const [currentFormIndex, setCurrentFormIndex] = useState(null);
   const [forms, setForms] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [answers, setAnswers] = useState([]);
   const [caracterizacionData, setCaracterizacionData] = useState({});
   const [isCompleted, setIsCompleted] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isCaracterizacionCompleted, setIsCaracterizacionCompleted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false); // Estado para controlar el envío
 
   useEffect(() => {
     fetchForms().then(data => {
@@ -22,12 +24,37 @@ function FormularioCompleted() {
     if (savedFormIndex !== null) {
       setCurrentFormIndex(JSON.parse(savedFormIndex));
     }
+
+    const savedAnswers = localStorage.getItem('formAnswers');
+    if (savedAnswers) {
+      setAnswers(JSON.parse(savedAnswers));
+    }
+
+    const savedQuestionIndex = localStorage.getItem('currentQuestionIndex');
+    if (savedQuestionIndex) {
+      setCurrentQuestionIndex(JSON.parse(savedQuestionIndex));
+    }
+
+    const savedCaracterizacionData = localStorage.getItem('caracterizacionData');
+    if (savedCaracterizacionData) {
+      setCaracterizacionData(JSON.parse(savedCaracterizacionData));
+      setIsCaracterizacionCompleted(true);
+    }
+
+    const isFormCompleted = localStorage.getItem('isFormCompleted');
+    if (isFormCompleted && JSON.parse(isFormCompleted) === true) {
+      setIsCompleted(true);
+    }
   }, []);
 
   const handleCaracterizacionSubmit = (data) => {
-    const tipoIndex = data.companyType === 'micro_pequeñas' ? 0 : data.companyType === 'unidad_negocio_productivo' ? 1 : data.companyType === 'medianas_grandes' ? 2 : data.companyType === 'excelencia_clinica' ? 3 : null;
+    const tipoIndex = data.companyType === 'micro_pequeñas' ? 0 : 
+                      data.companyType === 'unidad_negocio_productivo' ? 1 : 
+                      data.companyType === 'medianas_grandes' ? 2 : 
+                      data.companyType === 'excelencia_clinica' ? 3 : null;
     setCurrentFormIndex(tipoIndex);
     setCaracterizacionData(data);
+    setIsCaracterizacionCompleted(true);
     localStorage.setItem('currentFormIndex', JSON.stringify(tipoIndex));
     localStorage.setItem('caracterizacionData', JSON.stringify(data));
   };
@@ -37,55 +64,96 @@ function FormularioCompleted() {
     const answer = question.answers.find(a => a.id === answerId);
     if (!answer) return;
 
-    const newAnswers = [...answers, { questionId: question.id, answerId: answer.id }];
-    setAnswers(newAnswers);
-    localStorage.setItem('formAnswers', JSON.stringify(newAnswers));
+    const newAnswer = {
+      questionId: question.id,
+      answerId: answer.id,
+      value: answer.value,
+      category: question.category,
+      answers_count: question.answers_count,
+      questionText: question.text,
+      answerText: answer.text,
+    };
+
+    setSelectedAnswer(answerId);
+    setAnswers(current => {
+      const updatedAnswers = [...current, newAnswer];
+      localStorage.setItem('formAnswers', JSON.stringify(updatedAnswers));
+      localStorage.setItem('currentQuestionIndex', JSON.stringify(currentQuestionIndex + 1));
+      return updatedAnswers;
+    });
   };
 
   const handleNavigation = (direction) => {
-    if (direction === 'next' && currentQuestionIndex < forms[currentFormIndex].questions.length - 1) {
-      setCurrentQuestionIndex(current => current + 1);
+    if (direction === 'next' && selectedAnswer !== null) {
+      if (currentQuestionIndex < forms[currentFormIndex]?.questions.length - 1) {
+        setCurrentQuestionIndex(current => current + 1);
+        setSelectedAnswer(null);
+      } else {
+        setIsSubmitting(true); // Deshabilitar el botón de envío
+        const submissionData = {
+          answers: answers,
+          info: caracterizacionData
+        };
+        submitForm(forms[currentFormIndex].title, caracterizacionData.userName, caracterizacionData.email, submissionData)
+          .then(() => {
+            setIsCompleted(true);
+            localStorage.setItem('formAnswers', JSON.stringify(answers));
+            localStorage.setItem('isFormCompleted', 'true');
+            localStorage.removeItem('currentQuestionIndex');
+            localStorage.removeItem('caracterizacionData');
+          })
+          .catch(error => {
+            console.error('Error al enviar el formulario:', error);
+          })
+          .finally(() => {
+            setIsSubmitting(false); // Habilitar el botón de envío
+          });
+      }
     } else if (direction === 'previous' && currentQuestionIndex > 0) {
       setCurrentQuestionIndex(current => current - 1);
-    } else if (direction === 'next' && currentQuestionIndex === forms[currentFormIndex].questions.length - 1) {
-      setIsSubmitting(true);
-      submitForm(forms[currentFormIndex].title, caracterizacionData, answers)
-        .then(() => {
-          setIsCompleted(true);
-          localStorage.setItem('isFormCompleted', 'true');
-        })
-        .catch(error => {
-          console.error('Submission error', error);
-        })
-        .finally(() => {
-          setIsSubmitting(false);
-        });
+      setSelectedAnswer(null);
+      localStorage.setItem('currentQuestionIndex', JSON.stringify(currentQuestionIndex - 1));
+      setAnswers(current => {
+        const updatedAnswers = current.slice(0, -1);
+        localStorage.setItem('formAnswers', JSON.stringify(updatedAnswers));
+        return updatedAnswers;
+      });
     }
   };
 
   const handleRestart = () => {
     setIsCompleted(false);
-    setCurrentFormIndex(null);
     setCurrentQuestionIndex(0);
+    setSelectedAnswer(null);
     setAnswers([]);
-    localStorage.clear(); // Be cautious with this in a real app; you might clear more than intended
+    setIsCaracterizacionCompleted(false);
+    setCaracterizacionData({});
+    localStorage.removeItem('formAnswers');
+    localStorage.removeItem('currentQuestionIndex');
+    localStorage.removeItem('isFormCompleted');
+    localStorage.removeItem('caracterizacionData');
   };
 
-  if (currentFormIndex === null) {
-    return <Caracterizacion onFormSubmit={handleCaracterizacionSubmit} />;
-  } else if (!isCompleted) {
-    return (
-      <Question
-        form={forms[currentFormIndex]}
-        currentQuestionIndex={currentQuestionIndex}
-        onSelectAnswer={handleAnswerSelect}
-        onNavigate={handleNavigation}
-        isSubmitting={isSubmitting}
-      />
-    );
-  } else {
-    return <FormCompletion answers={answers} onRestart={handleRestart} />;
-  }
+  return (
+    <>
+      {!isCompleted && !isCaracterizacionCompleted ? (
+        <Caracterizacion onFormSubmit={handleCaracterizacionSubmit} />
+      ) : !isCompleted && isCaracterizacionCompleted ? (
+        forms.length > 0 && (
+          <Question
+            form={forms[currentFormIndex]}
+            currentQuestionIndex={currentQuestionIndex}
+            selectedAnswer={selectedAnswer}
+            onSelectAnswer={handleAnswerSelect}
+            onNavigate={handleNavigation}
+            isSubmitting={isSubmitting}
+          />
+        )
+      ) : (
+        <FormCompletion answers={answers} onRestart={handleRestart} />
+      )}
+    </>
+  );
 }
 
 export default FormularioCompleted;
