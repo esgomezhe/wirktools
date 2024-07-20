03f68f2971195716df94a00d6a59e0830ae437bb
Esteban Gomez <esgomezhe@unal.edu.co>
2024-07-20 16:35:51 -0500

    Cambios menores

--- forms/views.py
+++ forms/views.py
@@ -2,7 +2,7 @@ from .models import Form, CompletedForm
 from .serializers import FormSerializer, CompletedFormSerializer
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -10,7 +10,7 @@ from rest_framework.decorators import action
 class FormViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class CompletedFormViewSet(viewsets.ModelViewSet):
     queryset = CompletedForm.objects.all()
